ab08be4671900be6b8c4b80af83f6b4723a40f6f
iDoyoung <ido.young@icloud.com>
2022-07-26 15:24:01 +0900

    Implement Login view UI

--- StopWatch/Sources/Scenes/Login/LoginViewController.swift
+++ StopWatch/Sources/Scenes/Login/LoginViewController.swift
@@ -7,23 +7,56 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+final class LoginViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Stop Watch"
+        label.font = UIFont.systemFont(ofSize: 30, weight: .black)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Google Login", for: .normal)
+        button.setTitleColor(UIColor.label, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    //MARK: - Life cycle
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
     }
-    */
-
+    
+    override func loadView() {
+        super.loadView()
+        view.backgroundColor = .systemBackground
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUIComponents()
+    }
+    
+    private func setupUIComponents() {
+        view.addSubview(titleLabel)
+        view.addSubview(loginButton)
+        setupLayoutConstraint()
+    }
+    
+    private func setupLayoutConstraint() {
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: view.centerYAnchor),
+            loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loginButton.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 30)
+        ])
+    }
+    
 }
